0c8de9f752751ebd666c4bd9b5e927fb144f3117
Kelemen <murk9939@gmail.com>
2024-01-28 20:18:28 +0100

    Commitng

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = String::new();
+
+    println!("Plese enter a value for x: ");
+
+    io::stdin()
+        .read_line(&mut x)
+        .expect("Failed to read line");
+
+    let x: i32 = x.trim().parse()
+        .expect("Please type a number!");
+
+    println!("Szevasz te!, {}", x);
 }
