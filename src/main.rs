146ffe783eec2ab60347a27815c1ecdd54484570
Kelemen <murk9939@gmail.com>
2024-01-31 21:45:31 +0100

    Removing some messed up stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::io;
-use rand::Rng;
 
 fn main() {
     let mut x = String::new();
     let mut y = String::new();
-    let random_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Adj meg egy számot az x-nek: ");
     io::stdin().read_line(&mut x).expect("Failed to read line");
@@ -16,6 +14,4 @@ fn main() {
 
     let eredmény = x + y;
     println!("Az eredménye az x-nek meg az y-nak összeadva: {}", eredmény);
-
-    println!("A random szám: {}", random_number);
 }
