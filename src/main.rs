6f4a15ca067ba007d9c49b938271d9c5badb1c53
Kelemen <murk9939@gmail.com>
2024-02-07 17:57:02 +0100

    Fixed some more messed up stuff

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,32 @@ fn subtraction(x: i32, y: i32) -> i32 {
 }
 
 fn main() {
+    println!("Type in a value for x, then for y, and finally for the operation (addition, subtraction):");
+    let mut x = String::new();
+    io::stdin().read_line(&mut x).expect("Failed to read line!");
+    let x: i32 = x.trim().parse().expect("Please type a number!");
+
+    let mut y = String::new();
+    io::stdin().read_line(&mut y).expect("Failed to read line!");
+    let y: i32 = y.trim().parse().expect("Please type a number!");
+
+    let mut operation = String::new();
+    io::stdin().read_line(&mut operation).expect("Failed to read line!");
+
+    let eredmény = match operation.trim() {
+        "addition" => addition(x, y),
+        "subtraction" => subtraction(x, y),
+        _=> panic!("Unkonwn operation!"),
+    };
+
+    println!("The result: {}", eredmény);
+
+    let function_pointer: fn() = hungary;
+
+    function_pointer();
+}
+
+fn hungary() {
     println!("Adj egy értéket az x-nek, majd az y-nak, végül az operációnak (összeadás, kivonás):");
     let mut x = String::new();
     io::stdin().read_line(&mut x).expect("Nem sikerült beolvasni a sort!");
@@ -37,32 +63,6 @@ fn main() {
 
     println!("Az eredmény: {}", eredmény);
 
-    let function_pointer: fn() = english;
-
-    function_pointer();
-}
-
-fn english() {
-    println!("Type in a value for x, then for y, and finally for the operation (addition, subtraction):");
-    let mut x = String::new();
-    io::stdin().read_line(&mut x).expect("Failed to read line!");
-    let x: i32 = x.trim().parse().expect("Please type a number!");
-
-    let mut y = String::new();
-    io::stdin().read_line(&mut y).expect("Failed to read line!");
-    let y: i32 = y.trim().parse().expect("Please type a number!");
-
-    let mut operation = String::new();
-    io::stdin().read_line(&mut operation).expect("Failed to read line!");
-
-    let eredmény = match operation.trim() {
-        "addition" => addition(x, y),
-        "subtraction" => subtraction(x, y),
-        _=> panic!("Unkonwn operation!"),
-    };
-
-    println!("The result: {}", eredmény);
-    
     println!("Press enter to exit!/Nyomd meg az entert a kilépéshez!");
     io::stdin().read(&mut [0]).unwrap();
 }
