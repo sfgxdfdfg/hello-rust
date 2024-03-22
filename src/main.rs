aef4657c60252061179f43f1e64650fec9154bc2
sfgcdfdfg <murk9939@gmail.com>
2024-03-22 21:24:31 +0100

    Commiting

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::io;
 use languages::{english_main, magyar};
 use crate::info::info;
-use crate::ui::main_ui;
-mod ui;
 mod languages;
 mod info;
 pub mod operations;
@@ -10,14 +8,13 @@ pub mod operations;
 pub fn main() {
 
     let mut language = String::new();
-    println!("Type in the language you want to use (1-english, 2-magyar, i-info_menu):");
+    println!("Type in the language you want to use (1-english, 2-magyar, i-check_integer_types):");
     io::stdin().read_line(&mut language).expect("Failed to read line!");
     
     match language.trim() {
         "1" => english_main(),
         "2" => magyar(),
         "i" => info(),
-        "d" => main_ui().expect("Panic!"),
         _=> panic!("Unknown language!"),
     
     }
